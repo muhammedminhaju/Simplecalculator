1ee21d0ed963d9b3bde6295be64cc9bb202c46e1
Muhammed Minhaju A <muhammedminhaj25@gmail.com>
2022-12-01 18:17:22 +0530

    Create loop

--- calculator.py
+++ calculator.py
@@ -7,20 +7,28 @@ def mul(a,b):
 def div(a,b):
   print("Division ",a/b)
 def mod(a,b):
-  print("Modulo ",a%b)           
+  print("Modulo ",a%b)   
+s=input("Start a calculation then enter start\n Stop a calculation then enter stop")          
 a=input("Enter the 1st number ")
 a=int(a)
 op=input("Enter the Operator ")
 b=int(input("Enter the 2nd numder "))
-if(op=="+"):
-  sum(a,b)
-elif(op=="-"):
-  sub(a,b)  
-elif(op=="/"):
-  div(a,b)
-elif(op=="*"):
-  mul(a,b)  
-elif(op=="%"):
-  mod(a,b) 
+while (s=="start"):
+    if(op=="+"):
+      sum(a,b)
+    elif(op=="-"):
+     sub(a,b)  
+    elif(op=="/"):
+     div(a,b)
+    elif(op=="*"):
+     mul(a,b)  
+    elif(op=="%"):
+     mod(a,b) 
+    s=input("Start a calculation then enter start\nStop a calculation then enter stop") 
+    if(s=="start"):
+        a=input("Enter the 1st number ")
+        a=int(a)
+        op=input("Enter the Operator ")
+        b=int(input("Enter the 2nd numder ")) 
 
 
