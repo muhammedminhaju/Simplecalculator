74e981515aa8417a14378e2b8159fe1961ba51ef
Muhammed Minhaju A <muhammedminhaj25@gmail.com>
2022-12-01 18:04:57 +0530

    Just working

--- calculator.py
+++ calculator.py
@@ -1,5 +1,26 @@
-def inputcal():
-    a=int(input("Enter the 1st numder "))
-    op=input("Enter the Operator")
-    b=int(input("Enter the 2nd numder "))
-    return a,op,b
+def sum(a,b):
+  print("Addition ",a+b)
+def sub(a,b):
+  print("Subtraction ",a-b)
+def mul(a,b):
+  print("Multiplication ",a*b)
+def div(a,b):
+  print("Division ",a/b)
+def mod(a,b):
+  print("Modulo ",a%b)           
+a=input("Enter the 1st number ")
+a=int(a)
+op=input("Enter the Operator ")
+b=int(input("Enter the 2nd numder "))
+if(op=="+"):
+  sum(a,b)
+elif(op=="-"):
+  sub(a,b)  
+elif(op=="/"):
+  div(a,b)
+elif(op=="*"):
+  mul(a,b)  
+elif(op=="%"):
+  mod(a,b) 
+
+
