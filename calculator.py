2d8ceb82d580ef11752e2ac1b22e12dabddde17a
Muhammed Minhaju A <muhammedminhaj25@gmail.com>
2022-12-01 19:19:32 +0530

    create design2 function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,34 @@
+def design1():
+    line="----------------------------------------".center(40)
+    print("\n||{}||".format(line))
+    print("||               Calculator               ||")
+    print("||{}||".format(line)) 
+    print("||{}||".format("Start a calculation then enter 'start'".center(40)))
+    print("||{}||".format("Stop a calculation then enter 'stop'".center(40)))
+    s=input("||{}||=>".format(" ".center(40)))  
+    print("||{}||".format(line)) 
+    print("||{}||".format(s.center(40)))
+    print("||{}||".format(line)) 
+    return s
+def design2():
+    line="----------------------------------------".center(40)
+    print("||{}||".format(line))
+    a=input("||{}||=>".format("Enter the 1st number ".center(40)))
+    print("||{}||".format(line))
+    a=int(a)
+    print("||{}||".format("Enter the Operator".center(40)))
+    print("||{}||".format(" ".center(40)))
+    print("||{}||".format("* : Multiplication".center(40)))
+    print("||{}||".format("+ : Addition      ".center(40)))
+    print("||{}||".format("- : Subtraction   ".center(40)))
+    print("||{}||".format("/ : Division      ".center(40)))
+    print("||{}||".format("% : Modulo        ".center(40)))
+    op=input("||{}||=>".format(" ".center(40)))
+    print("||{}||".format(line))
+    b=int(input("||{}||=>".format("Enter the 2nd number ".center(40))))
+    print("||{}||".format(line))
+    return a,b,op
+  
 def sum(a,b):
   print("Addition ",a+b)
 def sub(a,b):
@@ -7,12 +38,13 @@ def mul(a,b):
 def div(a,b):
   print("Division ",a/b)
 def mod(a,b):
-  print("Modulo ",a%b)   
-s=input("Start a calculation then enter start\n Stop a calculation then enter stop")          
-a=input("Enter the 1st number ")
-a=int(a)
-op=input("Enter the Operator ")
-b=int(input("Enter the 2nd numder "))
+  print("Modulo ",a%b)  
+s=design1() 
+a,b,op=design2()       
+# a=input("Enter the 1st number ")
+# a=int(a)
+# op=input("Enter the Operator ")
+# b=int(input("Enter the 2nd numder "))
 while (s=="start"):
     if(op=="+"):
       sum(a,b)
@@ -24,7 +56,7 @@ while (s=="start"):
      mul(a,b)  
     elif(op=="%"):
      mod(a,b) 
-    s=input("Start a calculation then enter start\nStop a calculation then enter stop") 
+    s=design1()
     if(s=="start"):
         a=input("Enter the 1st number ")
         a=int(a)
